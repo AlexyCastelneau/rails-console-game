a01ec9b2c45b72ebe6fc1b3e794b8c5c04fd4930
Alexy Castelneau <alexy.castelneau@gmail.com>
2022-08-14 20:57:38 +0200

    game initialisation and start, game doesn't inherit of activerecord anymore, fixing perso initialisation, fix of 'Psych::DisallowedClass'

--- app/models/perso.rb
+++ app/models/perso.rb
@@ -5,18 +5,18 @@ class Perso < ApplicationRecord
   serialize :geared_stats, Hash
   serialize :current_stats, Hash
 
-  def set_base_stats(arguments) # à ajuster quand il y aura les races
+  def set_base_stats#(arguments) à ajuster quand il y aura les races
     self.base_stats = { "str" => 10, "dex" => 10, "vit" => 10, "int" => 10, "wis" => 10 }
   end
 
-  def set_gear(arguments) # à ajuster quand il y aura les classes
+  def set_gear#(arguments) à ajuster quand il y aura les classes
     # def generate_item(arguments) ?
-    head = Item.new_item({consumable: false, slot: "head", type: "light", dmg: 0, armor: 1, str: 0, dex: 0, vit: 0, int: 0, wis: 0 })
-    body = Item.new_item({consumable: false, slot: "body", type: "light", dmg: 0, armor: 1, str: 0, dex: 0, vit: 0, int: 0, wis: 0 })
-    hands = Item.new_item({consumable: false, slot: "hands", type: "light", dmg: 0, armor: 1, str: 0, dex: 0, vit: 0, int: 0, wis: 0 })
-    legs = Item.new_item({consumable: false, slot: "legs", type: "light", dmg: 0, armor: 1, str: 0, dex: 0, vit: 0, int: 0, wis: 0 })
-    feet = Item.new_item({consumable: false, slot: "feet", type: "light", dmg: 0, armor: 1, str: 0, dex: 0, vit: 0, int: 0, wis: 0 })
-    primary_weapon = Item.new_item({consumable: false, slot: "primary_weapon", type: "1H_sword", dmg: 5, armor: 0, str: 0, dex: 0, vit: 0, int: 0, wis: 0 })
+    head = Item.new_item({consumable: false, slot: "head", type: "light", dmg: 0, armor: 1, str: 0, dex: 0, vit: 0, int: 0, wis: 0, perso_id: self.id })
+    body = Item.new_item({consumable: false, slot: "body", type: "light", dmg: 0, armor: 1, str: 0, dex: 0, vit: 0, int: 0, wis: 0, perso_id: self.id })
+    hands = Item.new_item({consumable: false, slot: "hands", type: "light", dmg: 0, armor: 1, str: 0, dex: 0, vit: 0, int: 0, wis: 0, perso_id: self.id })
+    legs = Item.new_item({consumable: false, slot: "legs", type: "light", dmg: 0, armor: 1, str: 0, dex: 0, vit: 0, int: 0, wis: 0, perso_id: self.id })
+    feet = Item.new_item({consumable: false, slot: "feet", type: "light", dmg: 0, armor: 1, str: 0, dex: 0, vit: 0, int: 0, wis: 0, perso_id: self.id })
+    primary_weapon = Item.new_item({consumable: false, slot: "primary_weapon", type: "1H_sword", dmg: 5, armor: 0, str: 0, dex: 0, vit: 0, int: 0, wis: 0, perso_id: self.id })
     secondary_weapon = nil
     self.gear = { "head" => head, "body" => body, "hands" => hands, "legs" => legs, "feet" => feet,
         "primary_weapon" => primary_weapon, "secondary_weapon" => secondary_weapon }
@@ -28,7 +28,7 @@ class Perso < ApplicationRecord
     keys = gear.keys
     geared = { "armor" => 0, "str" => base["str"], "dex" => base["dex"], "vit" => base["vit"], "int" => base["int"], "wis" => base["wis"] }
     stats = geared.keys
-    keys.each { |slot| stats.each { |stat| geared[stat] += gear[slot][stat] } }
+    keys.each { |slot| (stats.each { |stat| geared[stat] += gear[slot].properties[stat] }) if !gear[slot].nil? }
     self.geared_stats = geared
   end
 
